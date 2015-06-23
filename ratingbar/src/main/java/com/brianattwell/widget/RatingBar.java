c64f6c367574593e7925231c76b26c5020b9a8ad
Brian Attwell <attwellBrian@gmail.com>
2015-06-23 02:48:35 -0700

    Rename widget project

--- ratingbar/src/main/java/com/brianattwell/widget/RatingBar.java
+++ ratingbar/src/main/java/com/brianattwell/widget/RatingBar.java
@@ -3,12 +3,18 @@ package com.brianattwell.widget;
 import android.content.Context;
 import android.util.AttributeSet;
 
+/**
+ * Variant of {@link android.widget.RatingBar} that measures its {@link #getIndeterminateDrawable}
+ * when determining its own size. You need to set the indeterminate drawable in order for this
+ * to work.
+ *
+ * See http://stackoverflow.com/a/30826146/1337257
+ */
 public class RatingBar extends android.widget.RatingBar {
 
     public RatingBar(Context context) {
         this(context, null);
     }
-
 
     public RatingBar(Context context, AttributeSet attrs) {
         super(context, attrs);
